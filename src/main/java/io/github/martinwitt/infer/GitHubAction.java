f95ff96875ca306047de75bbb64b24f3e1e016a4
Martin Wittlinger <wittlinger.martin@gmail.com>
2022-11-27 18:40:37 +0100

    add infer run command

--- src/main/java/io/github/martinwitt/infer/GitHubAction.java
+++ src/main/java/io/github/martinwitt/infer/GitHubAction.java
@@ -1,5 +1,13 @@
 package io.github.martinwitt.infer;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.function.Consumer;
 import org.kohsuke.github.GitHub;
 import io.quarkiverse.githubaction.Action;
 import io.quarkiverse.githubaction.Commands;
@@ -15,6 +23,38 @@ public class GitHubAction {
         System.out.println("Build command: " + buildCommand);
         System.out.println("I would run: " + "infer capture - " + buildCommand);
         System.out.println(context);
+        try {
+            int exitCode = runInfer("infer capture -- " + buildCommand);
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        System.out.println("Done running Infer ");
+    }
+
+    private int runInfer(String  command) throws IOException, InterruptedException {
+        ProcessBuilder builder = new ProcessBuilder();
+        builder.command(command);
+        builder.directory(new File(System.getProperty(".")));
+        Process process = builder.start();
+        StreamGobbler streamGobbler =
+                new StreamGobbler(process.getInputStream(), System.out::println);
+        Future<?> future = Executors.newSingleThreadExecutor().submit(streamGobbler);
+       return process.waitFor();
+    }
+    
+    private static class StreamGobbler implements Runnable {
+        private InputStream inputStream;
+        private Consumer<String> consumer;
+
+        public StreamGobbler(InputStream inputStream, Consumer<String> consumer) {
+            this.inputStream = inputStream;
+            this.consumer = consumer;
+        }
+
+        @Override
+        public void run() {
+            new BufferedReader(new InputStreamReader(inputStream)).lines().forEach(consumer);
+        }
     }
   }
 
