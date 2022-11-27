bcd0acaea64e17acfce63d71f7ecf5bcddd26bba
Martin Wittlinger <wittlinger.martin@gmail.com>
2022-11-27 12:16:41 +0100

    🚧 Add test workflow

--- src/main/java/io/github/martinwitt/infer/GitHubAction.java
+++ src/main/java/io/github/martinwitt/infer/GitHubAction.java
@@ -1,11 +1,15 @@
 package io.github.martinwitt.infer;
 
+import org.kohsuke.github.GitHub;
 import io.quarkiverse.githubaction.Action;
+import io.quarkiverse.githubaction.Commands;
+import io.quarkiverse.githubaction.Context;
+import io.quarkiverse.githubaction.Inputs;
 
 public class GitHubAction {
 
     @Action
-    void doSomething() {
+    void doSomething(Inputs inputs, Commands commands, Context context, GitHub gitHub) {
         System.out.println("Hello " + "From GitHub Action");
     }
   }
