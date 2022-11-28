545f6c9186be35d5d93c47ef5d3f84eb7c4020d9
Martin Wittlinger <wittlinger.martin@gmail.com>
2022-11-28 16:55:00 +0100

    keep constructor

--- src/main/java/io/github/martinwitt/infer/MyReflectionConfiguration.java
+++ src/main/java/io/github/martinwitt/infer/MyReflectionConfiguration.java
@@ -4,11 +4,12 @@ import com.contrastsecurity.sarif.ArtifactLocation;
 import com.contrastsecurity.sarif.PhysicalLocation;
 import com.contrastsecurity.sarif.Region;
 import com.contrastsecurity.sarif.Result;
+import com.contrastsecurity.sarif.Run;
 import com.contrastsecurity.sarif.SarifSchema210;
 import io.quarkus.runtime.annotations.RegisterForReflection;
 
 @RegisterForReflection(
-    targets = {SarifSchema210.class, Result.class,PhysicalLocation.class,ArtifactLocation.class,Region.class, 
+    targets = {SarifSchema210.class,Run.class, Result.class,PhysicalLocation.class,ArtifactLocation.class,Region.class, 
                 SarifSchema210.Version.class})
 public class MyReflectionConfiguration {
 }
