73aaf6ab27faa91bda7b7c9a94834671bb7c9e5e
Martin Wittlinger <wittlinger.martin@gmail.com>
2022-11-28 17:37:04 +0100

    keep constructor

--- src/main/java/com/contrastsecurity/sarif/EnumValueHack.java
+++ src/main/java/com/contrastsecurity/sarif/EnumValueHack.java
@@ -10,4 +10,12 @@ public class EnumValueHack {
   public com.contrastsecurity.sarif.SarifSchema210.Version getVersion() {
     return Version._2_1_0;
   }
+
+  public Object getLevel() {
+    Object error = com.contrastsecurity.sarif.Result.Level.ERROR;
+    Object warning = com.contrastsecurity.sarif.Result.Level.WARNING;
+    Object note = com.contrastsecurity.sarif.Result.Level.NOTE;
+    Object none = com.contrastsecurity.sarif.Result.Level.NONE;
+    return null;
+  }
 }
